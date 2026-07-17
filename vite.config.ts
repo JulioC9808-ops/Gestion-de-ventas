e15a4971ac3165f9e8385d8c265da73af7c6ed39
Julio_GE 08 <juliocesargiraudyescuedero2008@gmail.com>
2026-07-17 15:44:12 -0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && ].filter(Boolean),
+  plugins: [react(),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
